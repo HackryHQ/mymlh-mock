57f039cf71980f108695a8574d2329e774585cae
Jack Mousseau <jackmousseau@gmail.com>
2018-02-18 22:47:59 -0500

    Add scope key constants

--- src/lib/scopes.js
+++ src/lib/scopes.js
@@ -7,28 +7,34 @@ const DEFAULT_FIELDS = [
   'updated_at'
 ];
 
+const EMAIL_SCOPE = 'email';
 const EMAIL_FIELDS = [
   'email'
 ];
 
-const PHONE_NUMER_FIELDS = [
+const PHONE_NUMBER_SCOPE = 'phone_number';
+const PHONE_NUMBER_FIELDS = [
   'phone_number'
 ];
 
+const DEMOGRAPHICS_SCOPE = 'demographics';
 const DEMOGRAPHIC_FIELDS = [
   'gender'
 ];
 
+const BIRTHDAY_SCOPE = 'birthday';
 const BIRTHDAY_FIELDS = [
   'birthday'
 ];
 
+const EDUCATION_SCOPE = 'education';
 const EDUCATION_FIELDS = [
   'level_of_study',
   'major',
   'school'
 ];
 
+const EVENT_SCOPE = 'event';
 const EVENT_FIELDS = [
   'shirt_size',
   'dietary_restrictions',
@@ -47,16 +53,26 @@ const applyScopesToUser = function (scopes, completeUser) {
 
   addFields(DEFAULT_FIELDS);
 
-  scopes.includes('email') && addFields(EMAIL_FIELDS);
-  scopes.includes('phone_number') && addFields(PHONE_NUMER_FIELDS);
-  scopes.includes('demographics') && addFields(DEMOGRAPHIC_FIELDS);
-  scopes.includes('birthday') && addFields(BIRTHDAY_FIELDS);
-  scopes.includes('education') && addFields(EDUCATION_FIELDS);
-  scopes.includes('event') && addFields(EVENT_FIELDS);
+  scopes.includes(EMAIL_SCOPE) && addFields(EMAIL_FIELDS);
+  scopes.includes(PHONE_NUMBER_SCOPE) && addFields(PHONE_NUMBER_FIELDS);
+  scopes.includes(DEMOGRAPHICS_SCOPE) && addFields(DEMOGRAPHIC_FIELDS);
+  scopes.includes(BIRTHDAY_SCOPE) && addFields(BIRTHDAY_FIELDS);
+  scopes.includes(EDUCATION_SCOPE) && addFields(EDUCATION_FIELDS);
+  scopes.includes(EVENT_SCOPE) && addFields(EVENT_FIELDS);
 
   return user;
 };
 
 module.exports = {
-  applyScopesToUser: applyScopesToUser
+  applyScopesToUser: applyScopesToUser,
+  getAllScopes: function () {
+    return [
+      EMAIL_SCOPE,
+      PHONE_NUMBER_SCOPE,
+      DEMOGRAPHICS_SCOPE,
+      BIRTHDAY_SCOPE,
+      EDUCATION_SCOPE,
+      EVENT_SCOPE
+    ]
+  }
 };
